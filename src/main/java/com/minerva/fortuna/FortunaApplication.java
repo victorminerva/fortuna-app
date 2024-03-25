ce2cdc06c6ed9695c5431aff25f5110782960653
Victor Minerva <victorminerva.m@gmail.com>
2024-03-25 13:04:37 -0300

    Initial commit

---
+++ src/main/java/com/minerva/fortuna/FortunaApplication.java
@@ -0,0 +1,13 @@
+package com.minerva.fortuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minerva/fortuna/FortunaApplicationTests.java
@@ -0,0 +1,10 @@
+package com.minerva.fortuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunaApplicationTests {
+
+
+}
