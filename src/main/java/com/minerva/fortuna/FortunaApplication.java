3cf6144f6a3b9ed7d3aee62934d7c83042d12648
Victor Minerva <victorminerva.m@gmail.com>
2024-03-27 17:39:43 -0300

    Adding cache

--- src/main/java/com/minerva/fortuna/FortunaApplication.java
+++ src/main/java/com/minerva/fortuna/FortunaApplication.java
@@ -5,6 +5,7 @@ import com.minerva.fortuna.domain.Bet;
 import com.minerva.fortuna.domain.Paper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
+@EnableCaching
 public class FortunaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/minerva/fortuna/service/PaperService.java
+++ src/main/java/com/minerva/fortuna/service/PaperService.java
@@ -2,6 +2,8 @@ package com.minerva.fortuna.service;
 
 import com.minerva.fortuna.domain.Paper;
 import com.minerva.fortuna.repository.PaperRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +17,12 @@ public class PaperService {
         this.repository = repository;
     }
 
+    @Cacheable("PAPERS")
     public List<Paper> findAll() {
         return repository.findAll();
     }
 
+    @CacheEvict(value = "PAPERS", allEntries=true)
     public Paper create(final Paper paper) {
         return repository.save(paper);
     }
